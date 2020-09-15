67f987930a1afc25c70a10fef40f807552c3f38c
wuhuanxiong <wuhuanxiong@hofan.cn>
2020-09-15 20:11:29 +0800

    1、基础页面结构

---
+++ app/utils/helpers.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @Author: Whx
+ * @Date:   2020-09-15 18:01:40
+ * @Last Modified by:   Whx
+ * @Last Modified time: 2020-09-15 18:26:00
+ */
+
+function route_class {
+    return str_replace('.', '-', Route::currentRouteName());
+}
