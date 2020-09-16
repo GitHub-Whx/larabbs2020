91caff3113dc52d34d0601f956f2b217ff7cf6c2
wuhuanxiong <wuhuanxiong@hofan.cn>
2020-09-16 11:25:33 +0800

    helpers

--- app/utils/helpers.php
+++ app/utils/helpers.php
@@ -4,7 +4,7 @@
  * @Author: Whx
  * @Date:   2020-09-15 18:01:40
  * @Last Modified by:   Whx
- * @Last Modified time: 2020-09-16 11:20:35
+ * @Last Modified time: 2020-09-15 18:26:00
  */
 
 function route_class() {
