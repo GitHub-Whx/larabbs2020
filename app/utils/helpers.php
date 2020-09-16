c93bf92541386bec610e8ec9033b644cd99267ad
wuhuanxiong <wuhuanxiong@hofan.cn>
2020-09-16 11:19:02 +0800

    no message

--- app/utils/helpers.php
+++ app/utils/helpers.php
@@ -4,9 +4,9 @@
  * @Author: Whx
  * @Date:   2020-09-15 18:01:40
  * @Last Modified by:   Whx
- * @Last Modified time: 2020-09-15 18:26:00
+ * @Last Modified time: 2020-09-16 11:18:53
  */
 
-function route_class {
+function route_class() {
     return str_replace('.', '-', Route::currentRouteName());
 }
